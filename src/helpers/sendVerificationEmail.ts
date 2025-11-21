d752911a1fe64c6b182dada98f4583d3238a270e
shivsingh78 <shivsinghbaghel95@gmail.com>
2025-11-21 14:41:25 +0530

    create register page and also implement verification for authentic user

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,2 +1,27 @@
 import { resend } from"@/lib/resend"
 import VerificationEmail from "../../emails/VerificationEmail"
+import { ApiResponse } from "@/types/ApiRespnse"
+
+
+export async function sendVerificationEmail(
+     email:string,
+     username:string,
+     verifyCode: string,
+): Promise<ApiResponse>{
+     try {
+           await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: email,
+      subject: 'Shadow_talk | Verification code',
+      react: VerificationEmail({ username,otp:verifyCode })
+           })
+
+     return {success:true,message:"Verification email send successfully"}
+          
+     } catch (emailError) {
+          console.log("Error sending verification email",emailError)
+          return {success: false, message: 'Failed to send verification email '}
+          
+     }
+
+}
